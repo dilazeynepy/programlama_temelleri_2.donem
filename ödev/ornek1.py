25977bb37b36de66a973f386b0d6566928414cde
dilazeynepy <dilazeynepates86@gmail.com>
2025-02-12 15:59:00 +0300

    Add files via upload

---
+++ ödev/ornek1.py
@@ -0,0 +1,5 @@
+#haftanın günlerinden pazartesi ile başlayan ve cuma ile biten bir liste oluşturunuz.
+#oluşturduğunuz listenin indeksi 4 olan elamanını ekrana yazdırınız.
+
+hafta=["pazartesi","salı","çarşamba","perşembe","cuma"]
+print(hafta[4])
---
+++ ödev/ornek2.py
@@ -0,0 +1,4 @@
+ders=['K','O','D','L','A','M','A']
+print(ders[0])
+print(ders[2])
+print(ders[5])
